f0d198192a3d5efedccb9726c6a97c6add7809f6
sagar <chavansagar96044@gmail.com>
2022-07-26 17:16:55 +0530

    extend printMax method to print

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -2,12 +2,15 @@ package com.bridgelabz;
 
 public class FindMaximum<K extends Comparable<K>> {
 
-    K getMaximum(K n1, K n2, K n3, K n4){
+    void getMax(K n1, K n2, K n3, K n4){
         K max=n1;
         if (n2.compareTo(max) > 0) max=n2;
         if (n3.compareTo(max) > 0) max=n3;
         if (n4.compareTo(max) > 0) max=n4;
-        return max;
+        printMax(max);
+    }
+    void printMax(K value){
+        System.out.println("maximum= "+value);
     }
     public static void main(String[] args) {
 
@@ -15,18 +18,14 @@ public class FindMaximum<K extends Comparable<K>> {
 
         Integer num1,num2,num3,num4;
         num1=5;num2=9;num3=7;num4=3;
-        Integer maxInt=(Integer) maximum.getMaximum(num1,num2,num3,num4);
-        System.out.println("maximum Integer= "+maxInt);
+        maximum.getMax(num1,num2,num3,num4);
 
         Float f1,f2,f3,f4;
         f1=5.1f;f2=6.2f;f3=10.5f;f4=2.35f;
-        Float maxFloat=(Float) maximum.getMaximum(f1,f2,f3,f4);
-        System.out.println("maximum Float= "+maxFloat);
+        maximum.getMax(f1,f2,f3,f4);
 
         String s1,s2,s3,s4;
         s1="Apple"; s2="Peach"; s3="Banana"; s4="Mango";
-        String maxString= maximum.getMaximum(s1, s2, s3, s4).toString();
-        System.out.println("maximum String= "+maxString);
-
+        maximum.getMax(s1, s2, s3, s4);
     }
 }
